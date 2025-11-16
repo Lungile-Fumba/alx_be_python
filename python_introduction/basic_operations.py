b13d38343c596bdee9645da7f9fda7d5d78e17b5
Lungile Fumba <Lungilefumba@yahoo.com>
2025-11-16 18:19:19 +0200

    added simple intrest eqaution on basic_operation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,3 +4,12 @@ z = number1 + number2
 z1 = number1 * number2
 z2 = number1 - number2
 print(z), print(z1), print(z2)
+
+principal = 1000
+rate =0.05
+time = 3
+P = principal
+R = rate
+T = time
+I = P * R * T
+print(I)
