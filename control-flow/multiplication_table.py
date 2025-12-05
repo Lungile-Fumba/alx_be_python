1ae10384a6cbb822d56b63eb5772fd1f6b40e5ec
Lungile Fumba <Lungilefumba@yahoo.com>
2025-12-05 09:22:51 +0200

    add py file multiplication_table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,12 @@
+y = (1, 2, 3, 4, 5, 6, 7, 8, 9 , 10)
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+X = number
+
+for Y in y:
+
+    Z = X * Y
+
+    print( f"{X} * {Y} = {Z} " ) 
+
