819bc41c7caa8371cddbfb69011137ba95cafb92
Lungile Fumba <Lungilefumba@yahoo.com>
2025-12-05 09:44:33 +0200

    add range line 1 , fix bug

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,11 +1,9 @@
 
 number = int(input("Enter a number to see its multiplication table: "))
 
-X = number
-
 for Y in range(1, 11):
 
-    Z = X * Y
+    Z = number * Y
 
-    print( f"{X} * {Y} = {Z} " ) 
+    print( f"{number} * {Y} = {Z} " ) 
 
