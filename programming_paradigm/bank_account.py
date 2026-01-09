4af22101e34d3cdf499c4bfe4ff074e1e0c43258
Lungile Fumba <Lungilefumba@yahoo.com>
2026-01-09 22:12:21 +0200

    fix bugs on bank_account py file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,14 +6,14 @@ class BankAccount:
     
     def deposit(self, amount):
         self.account_balance += amount 
-        print( f"Deposited: ${amount}")
+        print( f"Deposited:${amount}")
         
     def withdraw(self, amount):
         self.account_balance -= amount
         if self.account_balance < 0: 
             return print("Insufficient Funds") 
-        print(f"Withdrew: ${amount}")
+        print(f"Withdrew:${amount}")
 
     def display_balance(self):
-        print (f"Current Balance: ${self.account_balance:.2f}")
+        print (f"Current Balance:${self.account_balance:.2f}")
 
