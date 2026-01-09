60b901fb5001aea036cea5c4d51bf26e7b41736e
Lungile Fumba <Lungilefumba@yahoo.com>
2026-01-09 21:46:38 +0200

    add bank_account and main-0 py files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount: 
+    def __init__(self, initial_balance):
+        if initial_balance < 0:
+           raise ValueError("Initial balance cannot be negative!")
+        self.account_balance = float(initial_balance)
+    
+    def deposit(self, ammount ):
+        self.account_balance += ammount 
+        print( f"Deposited: {ammount}")
+        
+    def withdraw(self, ammount ):
+        self.account_balance -= ammount
+        if self.account_balance < 0: 
+            return print("Insufficient Funds") 
+        print(f"Withdrew: {ammount}")
+
+    def display_balance(self ):
+        print (f"Current Balance: {self.account_balance}")
