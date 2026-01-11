45b1ac79708263930526c9cd266a2c3948348fbf
Lungile Fumba <Lungilefumba@yahoo.com>
2026-01-11 16:58:39 +0200

    fix bugs on bank_account py file

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -13,10 +13,10 @@ def main():
 
     if command == "deposit" and amount is not None:
         account.deposit(amount)
-        print(f"Deposited: ${amount:.2f}")
+        print(f"Deposited: ${amount:.1f}")
     elif command == "withdraw" and amount is not None:
         if account.withdraw(amount):
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.1f}")
         else:
             print("Insufficient funds.")
     elif command == "display":
