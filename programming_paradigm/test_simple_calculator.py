37575d303440fcf86ac8255276d5cc3d475a9ca4
lungile_fumba <lungilefumba@yahoo.com>
2026-01-19 11:23:34 +0200

    add test py and simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_addition(self):
+    def test_add(self):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
@@ -17,19 +17,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
 
-    def test_division(self):
+    def test_divide(self):
         """Test the division method."""
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertEqual(self.calc.divide(6, -3), -2)
         self.assertEqual(self.calc.divide(-6, -3), 2)
-        self.assertIsNone(self.calc.divide(6, 0))
-        
-if __name__ == '__main__':
-    unittest.main()
-    
+        self.assertIsNone(self.calc.divide(6, 0))  
