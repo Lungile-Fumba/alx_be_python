505b042f64cb5904d103038096185aa80308d8a1
lungile_fumba <lungilefumba@yahoo.com>
2026-01-20 11:51:56 +0200

    fix bug on robust

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,31 +3,39 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        self.is_checked_out = False
+    
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return True
+        return False
 
 
 class Library:
     
     def __init__(self):
         self.books = []
-        self.checked_out_books = {}  # Store checked out books by title
         
     def add_book(self, book):
         self.books.append(book)
         
     def check_out_book(self, title):
         for book in self.books:
-            if book.title == title:
-                self.books.remove(book)
-                self.checked_out_books[title] = book  # Save for later
+            if book.title == title and not book.is_checked_out:
+                book.check_out()
                 return True
         return False
     
     def return_book(self, title):
-        if title in self.checked_out_books:
-            self.books.append(self.checked_out_books[title])
-            del self.checked_out_books[title]
-        return
-    
-    def list_available_books(self):
         for book in self.books:
-            print(f"{book.title} by {book.author}")
+            if book.title == title and book.is_checked_out:
+                book.return_book()
+                return True
+        return False
